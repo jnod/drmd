604ddb43e61af00e806d7c19c91b205960cfdf8e
jnod <jnod117@gmail.com>
2016-04-20 15:32:34 -0500

    Change definitions

--- drmd.c
+++ drmd.c
@@ -13,8 +13,8 @@ int main() {
   bcm2835_i2c_begin();
   bcm2835_i2c_setSlaveAddress(0b00010100);
   bcm2835_i2c_set_baudrate(100000);
-  bcm2835_gpio_fsel(RPI2_GPIO_4, BCM2835_GPIO_FSEL_OUTP);
-  bcm2835_gpio_write(RPI2_GPIO_4, LOW);
+  bcm2835_gpio_fsel(RPI_PDN1, BCM2835_GPIO_FSEL_OUTP);
+  bcm2835_gpio_write(RPI_PDN1, HIGH);
 
   int count = 0;
   int sum = 0;
