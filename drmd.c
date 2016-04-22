6d7957d0cdd1df574635dad5cdb7def26b9dfe98
jnod <jnod117@gmail.com>
2016-04-22 00:18:50 -0500

    Refactor code

--- drmd.c
+++ drmd.c
@@ -1,47 +1,114 @@
 #include "drmd.h"
 
-static uint8_t buffer[2];
-static uint8_t result_code;
-static uint16_t voltage;
+static typedef enum { TRUE, FALSE } Bool;
+static typedef enum { MOVE_STEPPER, READ_UV, UI } State;
+
+static void   clean();
+static int    initialize();
+static void   interrupt(int);
+static State  machine(State);
+static State  stateMoveStepper();
+static State  stateReadUV(Bool);
+static State  stateUI();
+
+static Bool       sigint_set = FALSE;
+static uint8_t    buffer[2];
+static uint16_t   voltage;
 
 int main() {
+  if (initialize()) return 1; // exit if there was an error initializing
+  State state = UI;
+
+  while (1) {
+    if (sigint_set == TRUE) {
+      if (state == UI) break;
+      state = UI;
+      sigint_set = FALSE;
+    }
+    state = machine(state);
+  }
+
+  clean();
+  return 0;
+}
+
+static void clean() {
+  // revert to default state
+}
+
+static int initialize() {
   if (!bcm2835_init()) {
     printf("bcm2835_init failed. Are you running as root??\n");
     return 1;
   }
 
   bcm2835_i2c_begin();
-  bcm2835_i2c_setSlaveAddress(0b00010100);
   bcm2835_i2c_set_baudrate(100000);
-  buffer[0] = 0b10100000;
+
+  bcm2835_i2c_setSlaveAddress(0b00010100); // Select UV LED ADC
+  buffer[0] = 0b10100000; // Code to enable continuous calibration on ADC
   bcm2835_i2c_write(buffer, 1);
-  bcm2835_gpio_fsel(RPI_PDN1, BCM2835_GPIO_FSEL_OUTP);
-  bcm2835_gpio_write(RPI_PDN1, HIGH);
 
-  int count = 0;
-  int sum = 0;
+  bcm2835_gpio_fsel(PDN1, BCM2835_GPIO_FSEL_OUTP);
+  bcm2835_gpio_write(PDN1, HIGH); // Turn on UV LED
 
-  while (1) {
-    result_code = bcm2835_i2c_read(buffer, 2);
-
-    switch (result_code) {
-      case BCM2835_I2C_REASON_OK:
-        voltage = buffer[0] << 8 | buffer[1];
-        sum += voltage;
-        count++;
-        break;
-      case BCM2835_I2C_REASON_ERROR_NACK:
-      case BCM2835_I2C_REASON_ERROR_CLKT:
-      case BCM2835_I2C_REASON_ERROR_DATA:
-        break;
-    }
-
-    if (count >= 15) {
-      printf("%.1f mV\n", ((float)sum / count / 65536 * 1250));
-      count = 0;
-      sum = 0;
-    }
-  }
+  signal(SIGINT, interrupt); // Set interrupt(int) to handle Ctrl+c events
 
   return 0;
+}
+
+static void interrupt(int signo) {
+  if (signo == SIGINT) {
+    sigint_set = TRUE;
+  }
+}
+
+static State machine(State state) {
+  switch (state) {
+    case MOVE_STEPPER:
+      return stateMoveStepper();
+    case READ_UV:
+      return stateReadUV();
+    case UI:
+      return stateUI;
+  }
+}
+
+static State stateReadUV(Bool continuous) {
+  static int count = 0;
+  static int sum = 0;
+
+  bcm2835_i2c_setSlaveAddress(0b00010100); // Select UV LED ADC
+
+  switch (bcm2835_i2c_read(buffer, 2)) {
+    case BCM2835_I2C_REASON_OK:
+      voltage = buffer[0] << 8 | buffer[1];
+      sum += voltage;
+      count++;
+      break;
+    case BCM2835_I2C_REASON_ERROR_NACK:
+    case BCM2835_I2C_REASON_ERROR_CLKT:
+    case BCM2835_I2C_REASON_ERROR_DATA:
+      break;
+  }
+
+  if (count >= 15) {
+    printf("%.2f%%\n", ((float)sum / count / 65536 * 100));
+    count = 0;
+    sum = 0;
+
+    if (continuous == FALSE) return UI; 
+  }
+
+  return READ_UV;
+}
+
+static State stateMoveStepper() {
+  printf("(move stepper)");
+
+  return UI;
+}
+
+static State stateUI() {
+
 }
--- drmd.h
+++ drmd.h
@@ -2,40 +2,73 @@
 #define DRMD_H_
 
 #include <bcm2835.h>
+#include <signal.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
 
 // GPIO pin mappings to the bcm2835 for rpi b+ and rpi 2 40 pin headers
-#define RPI_GPIO_2     RPI_BPLUS_GPIO_J8_03
-#define RPI_GPIO_3     RPI_BPLUS_GPIO_J8_05
-#define RPI_GPIO_4     RPI_BPLUS_GPIO_J8_07
-#define RPI_GPIO_5     RPI_BPLUS_GPIO_J8_29
-#define RPI_GPIO_6     RPI_BPLUS_GPIO_J8_31
-#define RPI_GPIO_7     RPI_BPLUS_GPIO_J8_26
-#define RPI_GPIO_8     RPI_BPLUS_GPIO_J8_24
-#define RPI_GPIO_9     RPI_BPLUS_GPIO_J8_21
-#define RPI_GPIO_10    RPI_BPLUS_GPIO_J8_19
-#define RPI_GPIO_11    RPI_BPLUS_GPIO_J8_23
-#define RPI_GPIO_12    RPI_BPLUS_GPIO_J8_32
-#define RPI_GPIO_13    RPI_BPLUS_GPIO_J8_33
-#define RPI_GPIO_14    RPI_BPLUS_GPIO_J8_08
-#define RPI_GPIO_15    RPI_BPLUS_GPIO_J8_10
-#define RPI_GPIO_16    RPI_BPLUS_GPIO_J8_36
-#define RPI_GPIO_17    RPI_BPLUS_GPIO_J8_11
-#define RPI_GPIO_18    RPI_BPLUS_GPIO_J8_12
-#define RPI_GPIO_19    RPI_BPLUS_GPIO_J8_35
-#define RPI_GPIO_20    RPI_BPLUS_GPIO_J8_38
-#define RPI_GPIO_21    RPI_BPLUS_GPIO_J8_40
-#define RPI_GPIO_22    RPI_BPLUS_GPIO_J8_15
-#define RPI_GPIO_23    RPI_BPLUS_GPIO_J8_16
-#define RPI_GPIO_24    RPI_BPLUS_GPIO_J8_18
-#define RPI_GPIO_25    RPI_BPLUS_GPIO_J8_22
-#define RPI_GPIO_26    RPI_BPLUS_GPIO_J8_37
-#define RPI_GPIO_27    RPI_BPLUS_GPIO_J8_13
+#define GPIO2     RPI_BPLUS_GPIO_J8_03
+#define GPIO3     RPI_BPLUS_GPIO_J8_05
+#define GPIO4     RPI_BPLUS_GPIO_J8_07
+#define GPIO5     RPI_BPLUS_GPIO_J8_29
+#define GPIO6     RPI_BPLUS_GPIO_J8_31
+#define GPIO7     RPI_BPLUS_GPIO_J8_26
+#define GPIO8     RPI_BPLUS_GPIO_J8_24
+#define GPIO9     RPI_BPLUS_GPIO_J8_21
+#define GPIO10    RPI_BPLUS_GPIO_J8_19
+#define GPIO11    RPI_BPLUS_GPIO_J8_23
+#define GPIO12    RPI_BPLUS_GPIO_J8_32
+#define GPIO13    RPI_BPLUS_GPIO_J8_33
+#define GPIO14    RPI_BPLUS_GPIO_J8_08
+#define GPIO15    RPI_BPLUS_GPIO_J8_10
+#define GPIO16    RPI_BPLUS_GPIO_J8_36
+#define GPIO17    RPI_BPLUS_GPIO_J8_11
+#define GPIO18    RPI_BPLUS_GPIO_J8_12
+#define GPIO19    RPI_BPLUS_GPIO_J8_35
+#define GPIO20    RPI_BPLUS_GPIO_J8_38
+#define GPIO21    RPI_BPLUS_GPIO_J8_40
+#define GPIO22    RPI_BPLUS_GPIO_J8_15
+#define GPIO23    RPI_BPLUS_GPIO_J8_16
+#define GPIO24    RPI_BPLUS_GPIO_J8_18
+#define GPIO25    RPI_BPLUS_GPIO_J8_22
+#define GPIO26    RPI_BPLUS_GPIO_J8_37
+#define GPIO27    RPI_BPLUS_GPIO_J8_13
 
-#define RPI_SDA1       RPI_GPIO_2
-#define RPI_SCL1       RPI_GPIO_3
-#define RPI_PDN1       RPI_GPIO_4
+#define SDA1      GPIO2
+#define SCL1      GPIO3
+
+#define PDN1      GPIO4       // Absorbance LED
+#define PDN2      GPIO17      // Flourescence LED
+#define PDN3      GPIO21      // Peristaltic Pump
+
+/*  DRV8825 PINS:
+      PD      Input with internal PullDown resistor
+      PU      Input with internal PullUp resistor
+      PDPU    Input with internal PullDown and PullUp resistors
+      O       Output
+      OD      Output with an Open Drain
+*/
+#define nENBL     GPIO6       // PD     0, open = enable; 1 = disable
+#define STEP      GPIO12      // PD     rising edge steps motor
+#define DIR       GPIO13      // PD     0 = forward, 1 = reverse
+#define DECAY     GPIO16      // PDPU   0 = slow, open = mixed, 1 = fast
+#define nRESET    GPIO26      // PD     
+#define nSLEEP    GPIO20      // PD     0 = sleep, 1 = enable
+
+#define nHOME     GPIO11      // OD     0 when at home state, open otherwise
+#define nFAULT    GPIO19      // OD     0 when overtemp/overcurrent, open otherwise
+
+/*  [MODE2, MODE1, MODE0] determines the degree of microstepping
+      000       Full step
+      001       1/2 step
+      010       1/4 step
+      011       1/8 step
+      100       1/16 step
+      101-111   1/32 step
+*/
+#define MODE0     GPIO5       // PD
+#define MODE1     GPIO7       // PD
+#define MODE2     GPIO8       // PD
 
 #endif
